031b5434f883716f6fa121c72932fd632f11d086
Nicolas De Simone <nicolas.desimone@gmail.com>
2026-07-27 21:19:55 +0200

    feat(seo): add robots.txt, sitemap.xml, robots metadata directive

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,33 +1,12 @@
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://gerzatlive.desimone.fr';
-    const lastModified = new Date('2026-07-27');
+  const baseUrl = "https://gerzatlive.desimone.fr";
 
-    return [
-        {
-            url: baseUrl,
-            lastModified,
-            changeFrequency: 'daily',
-            priority: 1,
-        },
-        {
-            url: `${baseUrl}/app`,
-            lastModified,
-            changeFrequency: 'always',
-            priority: 0.95,
-        },
-        {
-            url: `${baseUrl}/app/carte`,
-            lastModified,
-            changeFrequency: 'always',
-            priority: 0.8,
-        },
-        {
-            url: `${baseUrl}/app/arrivees`,
-            lastModified,
-            changeFrequency: 'always',
-            priority: 0.75,
-        },
-    ];
+  return [
+    { url: baseUrl, lastModified: new Date(), changeFrequency: "daily", priority: 1.0 },
+    { url: `${baseUrl}/app`, lastModified: new Date(), changeFrequency: "hourly", priority: 0.9 },
+    { url: `${baseUrl}/app/carte`, lastModified: new Date(), changeFrequency: "hourly", priority: 0.8 },
+    { url: `${baseUrl}/app/arrivees`, lastModified: new Date(), changeFrequency: "hourly", priority: 0.8 },
+  ];
 }
